5ba3491bb0b8ee6a2333de71bbf2ed8d6c1eb467
kylec296 <kylec296@gmail.com>
2017-03-28 22:39:18 -0500

    Initial commit. Setting up website with typescript and react. Added redux, but havent set up yet with typings

---
+++ client/index.tsx
@@ -0,0 +1,13 @@
+/*
+ ./client/index.js
+ which is the webpack entry file
+ */
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { App } from "./components/App";
+
+ReactDOM.render(
+    <App compiler="TypeScript" framework="React" />,
+    document.getElementById("example")
+);
